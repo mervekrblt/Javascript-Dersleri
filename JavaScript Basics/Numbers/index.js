2cb74f6bf8e8b1aa4ba4367deaeda72a1cc63e06
Merve Karabulut <mervekarabulut@lonca.works>
2020-06-28 14:15:09 +0300

    Add numbers file & Use Math methods such as toFixed, floor, round etc.

---
+++ JavaScript Basics/Numbers/index.js
@@ -0,0 +1,15 @@
+const x = 15.123456789
+
+const y = x.toFixed(3)
+
+console.log(typeof(y))
+
+console.log(x.toPrecision(3))
+
+console.log(Math.round(x))
+
+console.log(Math.min(1, 50.7, -100))
+
+const z = Math.random()
+
+console.log(z)
