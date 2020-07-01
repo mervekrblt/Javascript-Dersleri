a0630d23079fa62842582a03a77fdb56fbd10322
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-01 16:38:31 +0300

    Add diagram exercise

---
+++ Javascript Basics (2nd)/If Else Diagram Exercise/index.js
@@ -0,0 +1,14 @@
+const person = prompt('who is there?')
+
+if (person === 'cancel') {
+    alert('Canceled');
+
+} else(person === 'admin');
+
+const password = prompt('password')
+if (password === 'cancel') {
+    alert('Canceled')
+
+} else(password === '123');{
+    alert('Welcome')
+}
