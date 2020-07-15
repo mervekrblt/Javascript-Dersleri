e49eddb341b3aa9c075103d790d0ea7734d6fa1f
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-15 17:26:43 +0300

    Add to do items

---
+++ To Do exercise/index.js
@@ -0,0 +1,44 @@
+// add querySelectors
+const form = document.querySelector('form')
+const input = document.querySelector('#txtTaskName')
+const btnDeleteAll = document.querySelector('#btnDeleteAll')
+const taskList = document.querySelector('#task-list')
+
+eventListeners()
+
+function eventListeners(){
+    form.addEventListener('submit', addNewItem)
+}
+
+// addNewItem function 
+
+function addNewItem(e){
+
+    if(input.value===''){
+        alert('Add new item')
+    }
+
+    //create li
+    const li = document.createElement('li')
+    li.className='list-group-item list-group-item-secondary'
+    li.appendChild(document.createTextNode(input.value))
+
+    //create a
+    const a = document.createElement('a')
+    a.classList= 'delete-item float-right'
+    a.setAttribute('href', '#')
+    a.innerHTML= '<i class="fas fa-times"></i>'
+
+    // a to li
+    li.appendChild(a)
+    
+    // add li to ul
+
+    taskList.appendChild(li)
+
+    //clear input
+    input.value=''
+
+    e.preventDefault()
+}
+
